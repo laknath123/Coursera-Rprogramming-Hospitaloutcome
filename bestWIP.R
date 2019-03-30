a19fb2c6a364189fd214e0c30ac79baa4dde8500
laknath123 <laknathdias@gmail.com>
2019-03-30 14:49:18 -0500

    created a function- need to add the error handling part and the one for pneumonia

--- bestWIP.R
+++ bestWIP.R
@@ -3,12 +3,12 @@ library(dplyr)
 
 #Reading only the required part of the datasert
 
-# wd<-"C:/Users/lakna/OneDrive/Desktop/Coursera/week 4"
-# setwd(wd)
+ wd<-"C:/Users/lakna/OneDrive/Desktop/Coursera/week 4"
+ setwd(wd)
 # list.files(wd)
 # 
 # 
-# datain<-fread("outcome-of-care-measures.csv")#,select =c("Provider Number","Hospital Name","State"))
+ datain<-fread("outcome-of-care-measures.csv")#,select =c("Provider Number","Hospital Name","State"))
 
 #code for throwing error message
 # state<-c("TX","MO","PA")
@@ -32,9 +32,26 @@ library(dplyr)
 
 
 
+best.R<-function(state=as.character(),condition=as.character()){
+   wd<-"C:/Users/lakna/OneDrive/Desktop/Coursera/week 4"
+   setwd(wd)
+  if (condition=="heart attack"){
+    datain<<-fread("outcome-of-care-measures.csv",select =c("Hospital Name","State","Hospital 30-Day Death (Mortality) Rates from Heart Attack"))
+    datain$`Hospital 30-Day Death (Mortality) Rates from Heart Attack`<-as.numeric(datain$`Hospital 30-Day Death (Mortality) Rates from Heart Attack`)
+    bestHospital<-datain%>%
+           arrange(`State`,`Hospital 30-Day Death (Mortality) Rates from Heart Attack`)%>%filter(State==state)
+     (bestHospital$`Hospital Name`)[1]
+  } else if (condition=="heart failure"){
+    datain<-fread("outcome-of-care-measures.csv",select=c("Hospital Name","State","Hospital 30-Day Death (Mortality) Rates from Heart Failure"))  
+    datain$`Hospital 30-Day Death (Mortality) Rates from Heart Failure`<-as.numeric(datain$`Hospital 30-Day Death (Mortality) Rates from Heart Failure`)
+    bestHospital<<-datain%>%arrange(`State`,`Hospital 30-Day Death (Mortality) Rates from Heart Failure`,`Hospital Name`)%>%filter(State==state)
+    (bestHospital$`Hospital Name`)[1]
+  }
+ 
+}
 
 
 
 
-
+best.R("TX", "heart attack")
 
