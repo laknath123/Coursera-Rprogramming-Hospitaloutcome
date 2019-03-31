e2954917be0ed7eccdfe15d6361eccc274f4b4ce
laknath123 <48140699+laknath123@users.noreply.github.com>
2019-03-31 17:58:27 -0500

    Update bestWIP.R

--- bestWIP.R
+++ bestWIP.R
@@ -1,10 +1,14 @@
+#2 Finding the best hospital in a state
+#This a function required to complete Programming Assignment 3 of Coursera R programming Course
+#Following is my solution to the problem
+
 library(data.table)
 library(stringr)
 library(dplyr)
 
 
 best<-function(state=as.character(),condition=as.character()){
-  setwd("C:/Users/lakna/OneDrive/Desktop/Coursera/week 4")
+  setwd("C:/Users/lakna/OneDrive/Desktop/Coursera/week 4") #set the path where the csv file resides in your computer 
   stlist<-fread("outcome-of-care-measures.csv",select=c("State"))
   st<-unique(stlist$State)
   a<-'Hospital 30-Day Death (Mortality) Rates from'
@@ -15,13 +19,12 @@ best<-function(state=as.character(),condition=as.character()){
   else if (state %in% st==FALSE){
     stop("invalid state")
   }
-  datain<<-fread("outcome-of-care-measures.csv",select =c("Hospital Name","State",paste(a,str_to_title(condition))))
-  #need to convert the 3rd column to 
-  bestHospital<-datain%>%filter(State==state)%>%
-    arrange(`State`,paste(a,str_to_title(condition),`Hospital Name`)) #This is where the problem is 
-  (bestHospital$`Hospital Name`)[1]
+  datain<-fread("outcome-of-care-measures.csv",select =c("Hospital Name","State",paste(a,str_to_title(condition))))
+  datain[,3]<-as.numeric(unlist(datain[,3]))
+    bestHospital<<-datain%>%filter(State==state)
+    bestHospital<-bestHospital[order( bestHospital[,3], bestHospital[,3] ),]
+      (bestHospital$`Hospital Name`)[1]
 }    
 
 
 best("TX","heart failure")
-
